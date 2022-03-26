d3edfbebf9a3ad1a4f9cbc1f406c9617a5315d0c
Alvaromedia <93778428+Alvaromedia@users.noreply.github.com>
2022-03-26 20:11:58 +0100

    Task 5.2

--- components/Chat.js
+++ components/Chat.js
@@ -1,10 +1,67 @@
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import { Bubble, GiftedChat } from 'react-native-gifted-chat';
+import {
+  View,
+  Text,
+  Button,
+  Platform,
+  KeyboardAvoidingView,
+} from 'react-native';
 
 export default class Chat extends Component {
-  render() {
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    };
+  }
+
+  componentDidMount() {
     let name = this.props.route.params.name;
 
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+        {
+          _id: 2,
+          text: `Welcome to the chat ${name}`,
+          createdAt: new Date(),
+          system: true,
+        },
+      ],
+    });
+  }
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
+
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: '#000',
+          },
+        }}
+      />
+    );
+  }
+
+  render() {
+    let name = this.props.route.params.name;
     this.props.navigation.setOptions({ title: name });
 
     let bgColor = this.props.route.params.bgColor;
@@ -13,11 +70,17 @@ export default class Chat extends Component {
       <View
         style={{
           flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
           backgroundColor: bgColor,
         }}
       >
+        <GiftedChat
+          renderBubble={this.renderBubble.bind(this)}
+          messages={this.state.messages}
+          onSend={messages => this.onSend(messages)}
+        />
+        {Platform.OS === 'android' ? (
+          <KeyboardAvoidingView behavior="height" />
+        ) : null}
         <Button
           title="Go Back"
           onPress={() => this.props.navigation.navigate('Start')}
